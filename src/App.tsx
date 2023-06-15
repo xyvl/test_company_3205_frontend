4ac1acd5df2d521b4a187c4919e93f24740dc631
Vlad Podshivalov <23091980luba@gmail.com>
2023-06-15 13:49:18 +0500

    end

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,100 @@
-import { useCustomSelector } from "./hooks/redux"
+import { useState } from "react";
+import { useCustomDispatch, useCustomSelector } from "./hooks/redux";
+import { getPost } from "./store/post";
+
+interface IUser {
+  email: string;
+  number: string;
+}
 
 function App() {
-  const state = useCustomSelector(state => state.state)
-  
+  const dispatch = useCustomDispatch();
+  const state = useCustomSelector((state) => state);
+
+  const [email, setEamil] = useState<string>("");
+  const [number, setNumber] = useState<string>("");
+  const [checkNumber, setCheckNumber] = useState<boolean>(true);
+
+  const user: IUser = {
+    email,
+    number: number.split("-").join(""),
+  };
+
+  const getUser = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (checkNumber === false) {
+      return;
+    }
+    dispatch(
+      getPost({ post: user, result: state.result, state: state.state })
+    );
+
+    setCheckNumber(true);
+  };
+
+  const onChangeNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let text: string = e.target.value;
+    const mask = () => {
+      let finnaly: string = text
+        .split("")
+        .reduce((accumulator, currentValue, index) =>
+          index % 2 === 0 || index === 0
+            ? (accumulator += `-${currentValue}`)
+            : (accumulator += `${currentValue}`)
+        );
+      return setNumber(finnaly);
+    };
+    if (text.length > 0) {
+      text = text.split("-").join("");
+      if (!!Number(text) === true) {
+        mask();
+        setCheckNumber(true);
+      } else {
+        mask();
+        setCheckNumber(false);
+      }
+    } else {
+      setNumber(text);
+      setCheckNumber(true);
+    }
+  };
   return (
-    <form>
-      <input type="text" /><input type="text" />
-    </form>
+    <div className="wrapper">
+      <form onSubmit={getUser}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEamil(e.target.value)}
+          placeholder="email"
+        />
+        <input
+          type="tel"
+          value={number}
+          onChange={onChangeNumber}
+          placeholder="number"
+        />
+        {checkNumber ? null : (
+          <p className="check_error">Вы ввели не правильный формат номера</p>
+        )}
+        <input
+          type="submit"
+          className={checkNumber ? undefined : "no_submit"}
+        />
+      </form>
+      {state.state === "" ? null : state.state === "loading" ? (
+        <div className="block">
+          <div className="circle"></div>
+        </div>
+      ) : state.state === "finish" ? (
+        <div className="block">
+          <p className="ok_result">{state.result}</p>
+        </div>
+      ) : (
+        <div className="block">
+          <p className="error_result">{state.result}</p>
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/store/post.ts
+++ src/store/post.ts
@@ -1,22 +1,68 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { store } from "./store";
 
-export const getPost = createAsyncThunk("post/getPost", async () => {
-  const data: IPost = { email: "ddddd", number: "fff" } ;
-  return data
-});
+let controller: any;
+const newTokent = async () => {
+  controller = await new AbortController();
+};
+newTokent();
+export const getPost = createAsyncThunk(
+  "post/getPost",
+  async (props: IInitialState, { rejectWithValue, dispatch }) => {
+    if (props.state === "") {
+      try {
+        const data: any = await axios.post(
+          `${process.env.REACT_APP_URL_SERVER}/api/get_user`,
+          props.post,
+          { signal: controller.signal }
+        );
+        return data.data;
+      } catch (e: any) {
+        return rejectWithValue(e.response.data);
+      }
+    } else if (props.state === "loading") {
+      await controller.abort();
+      await newTokent();
+      try {
+        const data: any = await axios.post(
+          `${process.env.REACT_APP_URL_SERVER}/api/get_user`,
+          props.post,
+          { signal: controller.signal }
+        );
+        return data.data;
+      } catch (e: any) {
+        return rejectWithValue(e.response.data);
+      }
+    } else {
+      try {
+        const data: any = await axios.post(
+          `${process.env.REACT_APP_URL_SERVER}/api/get_user`,
+          props.post,
+          { signal: controller.signal }
+        );
+        return data.data;
+      } catch (e: any) {
+        return rejectWithValue(e.response.data);
+      }
+    }
+  }
+);
 
 interface IPost {
-    email: string;
-    number: string;
+  email: string;
+  number: string;
 }
 
 interface IInitialState {
   state: "loading" | "finish" | "rejected" | "";
-  post: IPost
+  result: string;
+  post: IPost;
 }
 
 const initialState: IInitialState = {
   state: "",
+  result: "",
   post: {
     email: "",
     number: "",
@@ -34,10 +80,15 @@ const post = createSlice({
       })
       .addCase(getPost.fulfilled, (state, action) => {
         state.state = "finish";
-        state.post = action.payload;
+        state.result = action.payload;
       })
-      .addCase(getPost.rejected, (state) => {
-        state.state = "rejected";
+      .addCase(getPost.rejected, (state, action) => {
+        if (!action.payload) {
+          state.state = "loading";
+        } else {
+          state.result = action.payload as string;
+          state.state = "rejected";
+        }
       });
   },
 });
